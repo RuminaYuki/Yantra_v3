bc3457dfff977eb20371102b7fb9b206ab1661cd
SDFcl <pannachay2@gmail.com>
2026-09-09 13:40:20 +0700

    Finish Draw system

---
+++ Assets/Scripts/GamePlay/CombatSystem/DrawController.cs
@@ -0,0 +1,78 @@
+﻿using Unity.Mathematics;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class DrawController : MonoBehaviour
+{
+    [Header("Player Input")]
+    private InputSystem_Actions playerInput;
+
+    [Header("References")]
+    [SerializeField] SplineToLineRenderer splineToLineRenderer;
+    [SerializeField] Camera camera;
+    
+    [Header("Settings")]
+    [SerializeField] float angleThreshold = 10f;
+
+    private Vector2 lastMousePos;
+
+    private void Awake()
+    {
+        playerInput = new InputSystem_Actions();
+        if (camera == null) camera = Camera.main;
+    }
+
+    private void OnEnable()
+    {
+        playerInput.Enable();
+        if (playerInput != null)
+        {
+            playerInput.Player.MousePosition.performed += HandleStroke;
+        }
+    }
+
+    private void OnDisable()
+    {
+        playerInput.Disable();
+        if (playerInput != null)
+        {
+            playerInput.Player.MousePosition.performed -= HandleStroke;
+        }
+    }
+
+    private void HandleStroke(InputAction.CallbackContext context)
+    {
+        Vector2 mousePosition = context.action.ReadValue<Vector2>();
+        Vector2 mouseMovement = mousePosition - lastMousePos;
+
+        if (mouseMovement.sqrMagnitude > 0.01f)
+        {
+            Vector2 mouseDirection = mouseMovement.normalized;
+
+            Vector2 splineDirection =
+                splineToLineRenderer.GetSplineDirectionScreenSpace(camera);
+
+            float mouseAngle =
+                Mathf.Atan2(mouseDirection.y, mouseDirection.x) * Mathf.Rad2Deg;
+
+            float splineAngle =
+                Mathf.Atan2(splineDirection.y, splineDirection.x) * Mathf.Rad2Deg;
+
+            float angleDifference =
+                Mathf.Abs(Mathf.DeltaAngle(mouseAngle, splineAngle));
+
+            /*Debug.Log(
+                $"Mouse: {mouseAngle:F1}° | " +
+                $"Spline: {splineAngle:F1}° | " +
+                $"Difference: {angleDifference:F1}°"
+            );*/
+
+            if (angleDifference < angleThreshold)
+            {
+                splineToLineRenderer.AddProgress();
+            }
+        }
+
+        lastMousePos = mousePosition;
+    }
+}
---
+++ Assets/Scripts/GamePlay/CombatSystem/SplineToLineRenderer.cs
@@ -0,0 +1,80 @@
+﻿using UnityEngine;
+using UnityEngine.Splines;
+using static UnityEngine.Rendering.DebugUI;
+
+[ExecuteInEditMode]
+public class SplineToLineRenderer : MonoBehaviour
+{
+    [Header("References")]
+    [SerializeField] SplineContainer splineContainer;
+    [SerializeField] LineRenderer lineRenderer;
+
+    [Header("Settings")]
+    [Range(10, 100)]
+    [SerializeField] int resolutions = 30;
+    [Range(0.01f, 100f)]
+    [SerializeField] float nextKnotProgressDelta = 0.01f;
+    [SerializeField] float offset = 0f;
+
+    [Header("Debug")]
+    [Range(0f, 1f)]
+    [SerializeField] float currentProgress = 0f;
+    public float CurrentProgress 
+    {
+        get => Mathf.Clamp01(currentProgress + offset);
+        set => currentProgress = Mathf.Clamp01(value + offset);
+    }
+
+    [SerializeField] Vector3 currentKnotPosition;
+    public Vector3 CurrentKnotPosition
+    {
+        get => splineContainer.EvaluatePosition(CurrentProgress);
+    }
+
+    [SerializeField] Vector3 nextKnotPosition;
+    public Vector3 NextKnotPosition
+    {
+        get
+        {
+            float nextProgress = Mathf.Clamp01(CurrentProgress + nextKnotProgressDelta);
+            return splineContainer.EvaluatePosition(nextProgress);
+        }
+    }
+
+    void Update()
+    {
+        if (splineContainer == null || lineRenderer == null)
+            return;
+
+        // จำนวนจุดที่ต้องใช้ในการวาดตาม Progress
+        int pointCount = Mathf.Max(2, Mathf.CeilToInt(resolutions * CurrentProgress));
+
+        lineRenderer.positionCount = pointCount;
+        for (int i = 0; i < pointCount; i++)
+        {
+            // คำนวณ t ให้อยู่ตั้งแต่ 0 ถึง progress
+            float t = CurrentProgress * ((float)i / (pointCount - 1));
+
+            Vector3 position = splineContainer.EvaluatePosition(t);
+
+            lineRenderer.SetPosition(i, position);
+        }
+    }
+
+    #region API
+    public void SetProgress(float progress) => CurrentProgress = progress;
+    public void AddProgress() => CurrentProgress += nextKnotProgressDelta;
+    public float GetProgress() => CurrentProgress;
+    public Vector2 GetSplineDirectionScreenSpace(Camera cam)
+    {
+        Vector3 currentWorld = CurrentKnotPosition;
+        Vector3 nextWorld = NextKnotPosition;
+
+        Vector3 currentScreen = cam.WorldToScreenPoint(currentWorld);
+        Vector3 nextScreen = cam.WorldToScreenPoint(nextWorld);
+
+        return (new Vector2(nextScreen.x, nextScreen.y) -
+                new Vector2(currentScreen.x, currentScreen.y)).normalized;
+    }
+    #endregion
+}
