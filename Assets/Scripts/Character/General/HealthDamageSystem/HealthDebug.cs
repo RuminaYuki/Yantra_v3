a22353c946fa64efaf112941413bfbfff6fe2015
SDFcl <pannachay2@gmail.com>
2026-09-10 19:17:48 +0700

    Finish Block system

--- Assets/Scripts/Character/General/HealthDamageSystem/HealthDebug.cs
+++ Assets/Scripts/Character/General/HealthDamageSystem/HealthDebug.cs
@@ -3,8 +3,6 @@ using UnityEngine;
 [RequireComponent(typeof(Health))]
 public class HealthDebug : MonoBehaviour
 {
-    public bool IgnoreDamage = false;
-
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.F1))
@@ -19,14 +17,6 @@ public class HealthDebug : MonoBehaviour
             if (TryGetComponent<IDamageable>(out IDamageable damageable))
             {
                 damageable.TakeDamage(1f);
-            }
-        }
-        if (Input.GetKeyDown(KeyCode.F3))
-        {
-            if (TryGetComponent<Health>(out Health _health))
-            {
-                IgnoreDamage = !IgnoreDamage;
-                _health.SetEnableIgnoreDamage(IgnoreDamage);
             }
         }
     }
