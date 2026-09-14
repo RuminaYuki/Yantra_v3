0e71c7215b8444f434adef5e3902aaf21a4b9eff
SDFcl <pannachay2@gmail.com>
2026-09-14 22:30:45 +0700

    Skill point event channel

--- Assets/Scripts/GamePlay/GunSystem/SkillPoints.cs
+++ Assets/Scripts/GamePlay/GunSystem/SkillPoints.cs
@@ -1,8 +1,11 @@
+using Unity.Mathematics;
 using UnityEngine;
 
 public class SkillPoints : MonoBehaviour
 {
     [SerializeField] float _skillPoints = 0f;
+    [SerializeField] float _maxPoint = 10f;
+    [SerializeField] VoidEventChannelSO _onMaxPoint;
 
     public float CurrentSkillPoints => _skillPoints;
 
@@ -10,6 +13,11 @@ public class SkillPoints : MonoBehaviour
     {
         amount = amount < 0 ? -amount : amount;
         _skillPoints += amount;
+        _skillPoints = Mathf.Clamp(_skillPoints, 0f, _maxPoint);
+        if (_onMaxPoint != null )
+        {
+            if (_skillPoints >=  _maxPoint) _onMaxPoint.Raise();
+        }
     }
 
     public bool consume(float amount)
@@ -18,8 +26,10 @@ public class SkillPoints : MonoBehaviour
         if (_skillPoints >= amount)
         {
             _skillPoints -= amount;
+            _skillPoints = Mathf.Clamp(_skillPoints, 0f, _maxPoint);
             return true;
         }
         return false;
     }
+
 }
